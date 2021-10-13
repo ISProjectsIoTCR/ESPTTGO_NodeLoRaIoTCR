a21c1ae098d36e5a8f6827dddb0537121fb38af1
rrojasn <rrojasn17@gmail.com>
2021-10-13 08:24:14 -0600

    Display inhabilitado

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "user-variables.h"
 #include <SPI.h>
 #include <LoRa.h>    // https://github.com/sandeepmistry/arduino-LoRa
-#include <U8g2lib.h> // https://github.com/olikraus/U8g2_Arduino
+//#include <U8g2lib.h> // https://github.com/olikraus/U8g2_Arduino
 // #include <U8x8lib.h>
 
 #define OFF 0 // For LED
@@ -19,9 +19,9 @@
 #define LORA_IRQ 26  // GPIO26 -     SX1276 IRQ (interrupt request)
 
 // I2C OLED Display works with SSD1306 driver
-#define OLED_SDA 4
-#define OLED_SCL 15
-#define OLED_RST 16
+//#define OLED_SDA 4
+//#define OLED_SCL 15
+//#define OLED_RST 16
 
 /* Pick One. Hardware I2C does NOT work! This article helped: https://robotzero.one/heltec-wifi-kit-32/
   TTGo boards similar to Heltec boards, LED_BUILTIN = 2 instead of pin 25
@@ -32,12 +32,12 @@
 // UNCOMMENT one of the constructor lines below
 //U8X8_SSD1306_128X64_NONAME_SW_I2C Display(/* clock=*/ OLED_SCL, /* data=*/ OLED_SDA, /* reset=*/ OLED_RST); // Unbuffered, basic graphics, software I2C
 //U8G2_SSD1306_128X64_NONAME_1_SW_I2C Display(U8G2_R0, /* clock=*/ OLED_SCL, /* data=*/ OLED_SDA, /* reset=*/ OLED_RST); // Page buffer, SW I2C
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C Display(U8G2_R0, /* clock=*/OLED_SCL, /* data=*/OLED_SDA, /* reset=*/OLED_RST); // Full framebuffer, SW I2C
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C Display(U8G2_R0, /* clock=*/OLED_SCL, /* data=*/OLED_SDA, /* reset=*/OLED_RST); // Full framebuffer, SW I2C
 
 const int blueLED = LED_BUILTIN;
 bool confirmation = false;
 int reenvio = 0;
-int sensor1 = 17;
+float sensor1 = 17;
 int waitCount = 0;
 int waitLapse = 30000; //Cuanto tiempo esperamos la confirmación de recibido
 String rssi = "";
@@ -58,9 +58,9 @@ void setup()
 
   Serial.println("LoRa Sender");
 
-  Display.begin();
-  Display.enableUTF8Print(); // enable UTF8 support for the Arduino print() function
-  Display.setFont(u8g2_font_ncenB10_tr);
+  //Display.begin();
+  //Display.enableUTF8Print(); // enable UTF8 support for the Arduino print() function
+  //Display.setFont(u8g2_font_ncenB10_tr);
 
   // Very important for SPI pin configuration!
   SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
@@ -150,14 +150,14 @@ void sendSensorValue()
   digitalWrite(blueLED, OFF); // Turn blue LED off
 
   // Display Info
-  Display.clearBuffer();
-  Display.setCursor(0, 12);
-  Display.print("Enviando");
-  Display.setCursor(0, 30);
-  Display.print("Paquete:");
-  Display.setCursor(0, 48);
-  Display.print("value: " + (String)sensor1);
-  Display.sendBuffer();
+  //Display.clearBuffer();
+  //Display.setCursor(0, 12);
+  //Display.print("Enviando");
+  //Display.setCursor(0, 30);
+  //Display.print("Paquete:");
+  //Display.setCursor(0, 48);
+  //Display.print("value: " + (String)sensor1);
+  //Display.sendBuffer();
   delay(5000);
   waitingResponse();
 }
@@ -193,14 +193,14 @@ void waitingResponse()
         rssi = LoRa.packetRssi();
 
         // Display Info
-        Display.clearBuffer();
-        Display.setCursor(0, 12);
-        Display.print("Confirmado");
-        Display.setCursor(0, 42);
-        Display.print("    '" + (String)packet + "'");
-        Display.setCursor(0, 58);
-        Display.print("RSSI " + rssi);
-        Display.sendBuffer();
+        //Display.clearBuffer();
+        //Display.setCursor(0, 12);
+        //Display.print("Confirmado");
+        //Display.setCursor(0, 42);
+        //Display.print("    '" + (String)packet + "'");
+        //Display.setCursor(0, 58);
+        //Display.print("RSSI " + rssi);
+        //Display.sendBuffer();
 
         digitalWrite(blueLED, OFF); // Turn blue LED off
         Serial.println(packet + "' con RSSI " + rssi);
